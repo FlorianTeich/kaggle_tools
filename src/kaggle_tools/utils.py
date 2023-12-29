eaed4a188a7cd45d0e15d5866f9c220998ccbaea
Florian Teich <florianteich@gmail.com>
2023-12-29 18:15:41 +0100

    fix

--- src/kaggle_tools/utils.py
+++ src/kaggle_tools/utils.py
@@ -19,7 +19,7 @@ def get_dataset(datasetname:str="spaceship-titanic"):
         donezip_path = dp_input / "unzip.done"
         Path(dp_input).mkdir(parents=True, exist_ok=True)
         if not os.path.exists(zipfile_path):
-            proc = subprocess.Popen(["kaggle competitions download -c " + datasetname + " -p " + str(dp_input)], stdout=subprocess.PIPE, shell=True)
+            proc = subprocess.Popen(["kaggle", "competitions", "download", "-c", datasetname, "-p", str(dp_input)], stdout=subprocess.PIPE, shell=True)
             proc.wait()
             if not os.path.exists(donezip_path):
                 with zipfile.ZipFile(zipfile_path, 'r') as zip_ref:
